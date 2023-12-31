8b7e163a3d800115403b87b83819acbfd9841cc6
Aakash Gupta <f20180887@pilani.bits-pilani.ac.in>
2023-12-31 13:26:12 +0530

    ASTVisitor implemented in main function

--- EfficientCompiler/main.cpp
+++ EfficientCompiler/main.cpp
@@ -11,12 +11,13 @@ import compiler;
 #include <memory>
 #include <cassert>
 #include <functional>
+#include <stdexcept>
 
 using namespace std;
 
 
-template<typename T>
-using P = std::pair<int, T>;
+template<CENonTerminal ENonTerminal, typename T>
+using P = std::pair<ENonTerminal, T>;
 
 struct start_parser
 {
@@ -59,18 +60,70 @@ struct expression_parser
 	}
 };
 
-template <IsParseTreeNode ptn, IsASTNode astn>
-constexpr unique_ptr<astn> ast_converter(unique_ptr<ptn> node, unique_ptr<astn> inherited)
-{
-	std::function<unique_ptr<astn>(unique_ptr<ptn>, unique_ptr<astn>)> converter = ast_converter<ptn, astn>;
 
-	if (node->node_type == ANonTerminal::start)
-		return start_parser::to_ast(converter, std::move(node), std::move(inherited));
-	
-	if (node->node_type == ANonTerminal::expression)
-		return expression_parser::to_ast(converter, std::move(node), std::move(inherited));
-	
-	assert(false);
+template<int array_size, typename... Types>
+class ASTVisitor
+{
+	using vtype = std::variant<Types...>;
+
+	template <IsParseTreeNode ptn, IsASTNode astn>
+	constexpr auto visitor(unique_ptr<ptn> node, unique_ptr<astn> inherited) const -> unique_ptr<astn>
+	{
+		if (!node)
+			return nullptr;
+
+		std::function<unique_ptr<astn>(unique_ptr<ptn>, unique_ptr<astn>)> converter =
+			[this](unique_ptr<ptn> node, unique_ptr<astn> inherited) -> unique_ptr<astn>
+			{
+				return this->visitor(std::move(node), std::move(inherited));
+			};
+
+		return std::visit(
+			[&](auto& _class_to_call) -> unique_ptr<astn>
+			{
+				using visitor_type = std::remove_cvref_t<decltype(_class_to_call)>;
+				return visitor_type::to_ast(converter, std::move(node), std::move(inherited));
+			},
+			m_visitors[static_cast<int>(node->node_type)]
+		);
+	}
+
+public:
+	const std::array<vtype, array_size> m_visitors;
+
+	template <IsParseTreeNode ptn, IsASTNode astn>
+	constexpr auto visit(unique_ptr<ptn> node) const -> unique_ptr<astn>
+	{
+		if (!node)
+			return nullptr;
+
+		return visitor<ptn, astn>(std::move(node), nullptr);
+	}
+};
+
+template<CENonTerminal ENonTerminal, typename... Types>
+constexpr auto build_visitor(std::pair<ENonTerminal, Types> ...args)
+{
+	using vtype = std::variant<Types...>;
+
+	constexpr auto array_size = 2; // get_enum_size<ENonTerminal>();
+	std::vector<std::pair<ENonTerminal, vtype>> arr{ args... };
+
+	std::array<vtype, array_size> m_visitors;
+	std::array<int, array_size> counts{};
+
+	for (auto& [key, value] : arr)
+		m_visitors[static_cast<int>(key)] = value, counts[static_cast<int>(key)]++;
+
+	for (auto& x : counts)
+		if (x == 0)
+			throw std::runtime_error("Mapping of visitor not found for atleast one Non Terminal.");
+		else if (x > 1)
+			throw std::runtime_error("There exist atleas two mappings for single Non Terminal.");
+
+	return ASTVisitor<array_size, Types...> {
+		.m_visitors = m_visitors
+	};
 }
 
 static auto read_file(string_view filename)
@@ -93,6 +146,8 @@ int main()
 	using LexerTypes = typename std::remove_cvref_t<decltype(*output2.root)>::LexerTypes;
 	using ENonTerminal = typename std::remove_cvref_t<decltype(*output2.root)>::ENonTerminal;
 
-	const auto res = ast_converter(move(output2.root), std::make_unique<ASTNode<LexerTypes, ENonTerminal>>());
-	cout << output2.root << endl;
+	build_visitor(
+		P{ANonTerminal::start, start_parser{}},
+		P{ANonTerminal::expression, expression_parser{}}
+	).visit<std::remove_cvref_t<decltype(*output2.root)>, ASTNode<LexerTypes, ENonTerminal>>(std::move(output2.root));
 }
